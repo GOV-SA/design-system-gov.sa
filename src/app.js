7bd086e65a052599e8fcaba1dd041298fdeea7ca
akramabdelrahman <a.akram@fixtag.com.sa>
2019-11-19 16:48:31 +0300

    Revert "landing page is done"
    This reverts commit ee41b5a6af77cbd0739fbee39a80820fe398a8ca.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-import '../node_modules/jquery/src/jquery';
-import 'popper.js';
+import'../node_modules/jquery/src/jquery';
+import'popper.js';
 import 'bootstrap';
 import '../src/lib/svgxuse'
 import './scss/style.scss';
-import './js/fileUpload';
--- webpack.config.js
+++ webpack.config.js
@@ -28,14 +28,8 @@ module.exports = function() {
         plugins: [
             new HtmlWebpackPlugin({
                 title: 'Webpack starter project',
-                template: path.resolve('./src/index.html'),
-                filename: 'index.html',
+                template: path.resolve('./src/index.html')
             }),
-            new HtmlWebpackPlugin({
-                title: 'شارك',
-                template: path.resolve('./src/landing.html'),
-                filename: 'landing.html',
-              }),
             new webpack.HotModuleReplacementPlugin()
         ],
         module: {
