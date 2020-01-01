80d0312921ca07154aebcc4dcbec8665c50b9523
reema-2 <reeemaa23@gmail.com>
2020-01-01 12:37:24 +0300

    added all components

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import'../node_modules/jquery/src/jquery';
-import'popper.js';
-import 'bootstrap';
-import './scss/components/search/search';
-import'./scss/components/tag/tag';
-import'./scss/components/file-upload/file-upload';
-import '../src/lib/svgxuse';
-import './scss/style.scss';
-import'./scss/components/side-navigation/side-navigation';
