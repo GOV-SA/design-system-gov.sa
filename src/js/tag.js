f5828dd17d1f00312b84e4f4eeb865028e47deda
reema-2 <reeemaa23@gmail.com>
2020-01-02 10:15:41 +0300

    fixed link radio dropdown tag.js and card

--- src/js/tag.js
+++ src/js/tag.js
@@ -1,6 +1,14 @@
-// to remove tag
-$(".colsed-tag").click(function() {
-  $(this)
-    .parent(".govsa-tag")
-    .remove();
-});
+// to remove tag javascript
+var buttons = document.getElementsByClassName("colsed-tag");
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    this.parentNode.remove();
+  });
+}
+
+// to remove tag jquery
+// $(".colsed-tag").click(function() {
+//   $(this).parent(".govsa-tag").remove();
+// });
+
+
